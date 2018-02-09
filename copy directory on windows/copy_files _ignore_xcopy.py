0acab7a98b5e3cb6105b099a3374397a0db2b1e8
Anubhav Apurva <anubhavh3@gmail.com>
2018-02-09 10:20:49 +0530

    Add files via upload

---
+++ copy directory on windows/copy_files _ignore_xcopy.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import time
+
+start_time = time.time()
+
+folders=["3.1.5"]
+
+f=open("copied_files.txt","w")
+
+excludelist="exclude.txt"
+
+for foldername in folders:
+    try:
+        dir_src = os.path.join(r"Z:\copied_folder",foldername)
+        dir_dst = os.path.join(r"C:\Projects\TestLogs\copied_folder",foldername)
+
+
+        #os.system('xcopy "%s" "%s" /i/c/s/q/y/j' % (dir_src, dir_dst))
+        #os.system('xcopy /exclude:"%s" "%s" "%s"' % (excludelist,dir_src, dir_dst))
+        os.system('xcopy /exclude:%s %s %s /i/c/s/y/j/q' % (excludelist,dir_src, dir_dst))
+        
+        print "copied folder : "+foldername
+        f.write(str(foldername)+"\n")
+        
+    except Exception as e:
+        print(e)
+        f.write(str(e)+"\n")
+        continue
+
+f.close()
+print "\n***Done Copying***\n"
+print("--- %s seconds ---" % (time.time() - start_time))
