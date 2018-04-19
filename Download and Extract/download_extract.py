b6de006106d2a7ac046f0d349a396bcb47e93444
Anubhav Apurva <anubhavh3@gmail.com>
2018-04-19 14:22:31 +0530

    Download and extract zip or tar files

---
+++ Download and Extract/download_extract.py
@@ -0,0 +1,37 @@
+import urllib2
+import os
+
+'''Download'''
+
+print("Downloading...")
+
+destDir="data"
+os.mkdir(destDir)
+
+fileName="my_file.zip"
+filePath=os.path.join(destDir,fileName)
+
+url = 'https://example.tar.gz'
+f = urllib2.urlopen(url)
+
+with open(filePath, "wb") as code:
+    code.write(f.read())
+
+'''Extraxt tar'''
+
+print("Extracting...")
+import tarfile
+tar = tarfile.open(filePath)
+tar.extractall(path=destDir)
+tar.close()
+
+
+'''Extract zip'''
+
+print("Extracting...")
+from zipfile import ZipFile
+thefile=ZipFile(filePath)
+thefile.extractall(destDir)
+thefile.close()
+
+
