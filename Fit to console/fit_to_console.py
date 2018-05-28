d18261bb9a17266e70faba65f3e23da1b6f01088
Anubhav Apurva <anubhavh3@gmail.com>
2018-05-28 14:24:48 +0530

    Add files via upload

--- Fit to console/fit_to_console.py
+++ Fit to console/fit_to_console.py
@@ -1,4 +1,18 @@
 import os
 rows, columns = os.popen('stty size', 'r').read().split()
-print(' MENU '.center(int(columns), '*'))
-print(' MENU '.center(int(columns)))
+
+word="Hello!"
+
+##Center
+print("\n")
+print(word.center(int(columns), '*'))
+print("\n")
+print(word.center(int(columns)))
+print("\n")
+
+##Right
+print(('%'+str(int(columns)) +'s') %(word) )
+print("\n")
+
+
+##Left,Center,Right
